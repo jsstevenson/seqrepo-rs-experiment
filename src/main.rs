2157ad6532d47fbed0e277306d7f08c15ccdb360
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-04-02 09:13:50 -0400

    add stubs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,55 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{extract::Path, http::StatusCode, routing::get, Json, Router};
+use serde::Serialize;
+
+fn app() -> Router {
+    Router::new()
+        .route("/ping", get(get_ping))
+        .route("/sequence/{alias}", get(get_sequence))
+        .route("/metadata/{alias}", get(get_metadata))
+}
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app()).await.unwrap();
+}
+
+#[derive(Serialize)]
+struct PingResponse {
+    version: String,
+    url: String,
+}
+
+async fn get_ping() -> (StatusCode, Json<PingResponse>) {
+    let response = PingResponse {
+        version: String::from("zzzz"),
+        url: String::from("http://google.com"),
+    };
+    (StatusCode::OK, Json(response))
+}
+
+//  404 if not found
+//  422 if invalid request
+async fn get_sequence(Path(alias): Path<String>) -> (StatusCode, String) {
+    (StatusCode::OK, alias)
+}
+
+#[derive(Serialize)]
+struct MetadataResponse {
+    length: i64,
+    aliases: String,  // todo array
+    alphabet: String, // todo nullable
+    added: String,    // todo nullable
+}
+
+//  404 if not found
+//  422 if invalid request
+async fn get_metadata(Path(alias): Path<String>) -> (StatusCode, Json<MetadataResponse>) {
+    let response = MetadataResponse {
+        length: 10,
+        aliases: alias,
+        alphabet: String::from("zzz"),
+        added: String::from("zzz"),
+    };
+    (StatusCode::OK, Json(response))
 }
